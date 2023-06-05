d92212f3ffad83c4749899ea551e35849e06b4e0
Федор Надуткин <FMNadutkin@sberbank.ru>
2023-06-05 17:41:57 +0300

    [HW3]: Realisation

--- src/main/java/nadutkin/utils/Constants.java
+++ src/main/java/nadutkin/utils/Constants.java
@@ -1,6 +1,13 @@
 package nadutkin.utils;
 
+import nadutkin.database.impl.MemorySegmentDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Constants {
     public static final Long FLUSH_THRESHOLD_BYTES = (long) (1 << 26);
     public static final String REQUEST_PATH = "/v0/entity";
+    public static final Integer MAX_FAILS = 100;
+    public static final Logger LOG = LoggerFactory.getLogger(MemorySegmentDao.class);
+
 }
